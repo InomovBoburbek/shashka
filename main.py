cb47215d99d44c9e19a27ec2c4d790512ed93de7
bobur <valiyev7115@gmail.com>
2025-01-08 15:38:49 +0500

    message

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 def pawn(x1, y1, x2, y2):
-    return  y2 - y1 == 1 and abs(x2-x1) == 1
+    return y2-y1==1 and abs(x2-x1)==1
 
-def damka(x1,y1,x2,y2):
-    pass
+def damka(x1, y1, x2, y2):
+    ox = abs(x1 - x2)
+    oy = abs(y1 - y2)
 
- print(pawn(1,1,2,3))
+    return ox == oy
+
+print(damka(5, 1, 5, 2))
+
+
+# git add .
+# git commit -m "message"
+# git push origin master/main
